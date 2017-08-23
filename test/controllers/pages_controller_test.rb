f5eaed55d1c0646d6102760e62f809ae1afceb87
felipe vera <felipevera10@gmail.com>
2017-08-23 12:09:37 -0300

    primer commit

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get batman" do
+    get pages_batman_url
+    assert_response :success
+  end
+
+  test "should get superman" do
+    get pages_superman_url
+    assert_response :success
+  end
+
+  test "should get batman_vs_superman" do
+    get pages_batman_vs_superman_url
+    assert_response :success
+  end
+
+end
